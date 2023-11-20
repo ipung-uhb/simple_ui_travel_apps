83dcb522aed9682eb6fef092ba396dc1eabe0d84
ipung-uhb <purwono@uhb.ac.id>
2023-11-20 11:22:44 +0700

    Edit the Login Screen to add input form and link to register view

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,26 +1,52 @@
 // login_screen.dart
 import 'package:flutter/material.dart';
+import 'register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Add your login UI components here
-            Text('Login Screen'),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Navigate to the dashboard screen
+                // Add your login logic here
+                // For now, navigate to the dashboard screen
                 Navigator.pushReplacementNamed(context, '/dashboard');
               },
               child: Text('Login'),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Navigate to the register screen
+                Navigator.pushNamed(context, '/register');
+              },
+              child: Text('Don\'t have an account? Register here.'),
             ),
           ],
         ),
