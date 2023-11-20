7a9681737004b6eae403c213aa27424cc97cd799
ipung-uhb <purwono@uhb.ac.id>
2023-11-20 11:54:37 +0700

    Add card scroll and search input in dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -8,22 +8,96 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Add your dashboard UI components here
-            Text('Dashboard Screen'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Log out and navigate back to the login screen
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: Text('Logout'),
+      body: Column(
+        children: [
+          // Search form at the top
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search for a destination...',
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
-          ],
-        ),
+          ),
+          // Scrollable card view
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: [
+                // Example Travel Card
+                TravelCard(
+                  imageUrl: 'https://expertvagabond.com/wp-content/uploads/travel-photography-tips-guide-1.jpg',
+                  caption: 'Beautiful Beach',
+                  source: 'Photo by John Doe',
+                ),
+                TravelCard(
+                  imageUrl: 'https://expertvagabond.com/wp-content/uploads/travel-photography-tips-guide-1.jpg',
+                  caption: 'Beautiful Beach',
+                  source: 'Photo by John Doe',
+                ),
+                TravelCard(
+                  imageUrl: 'https://expertvagabond.com/wp-content/uploads/travel-photography-tips-guide-1.jpg',
+                  caption: 'Beautiful Beach',
+                  source: 'Photo by John Doe',
+                ),
+                // Add more TravelCard widgets for additional items
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TravelCard extends StatelessWidget {
+  final String imageUrl;
+  final String caption;
+  final String source;
+
+  TravelCard({
+    required this.imageUrl,
+    required this.caption,
+    required this.source,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 16.0),
+      elevation: 4.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.network(
+            imageUrl,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  caption,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  source,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
